3402927e21acca0ef55e94107786fb17dc6e06bf
ADEESA indumina <94174257+METRODENS@users.noreply.github.com>
2021-12-15 17:25:55 +0530

    Delete SQ-QA.js

--- SQ-QA.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-   METRO_BOT_OWNER: '94702506161,94718141853', // Adeesa Indumina - Metro Dens Owner
-   METRO_BOT_TEAM: '94702506161', // Team - METRO DENS TEAM 
-   QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship 
-    } 
