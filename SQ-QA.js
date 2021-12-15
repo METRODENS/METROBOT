479838adc87b829121708445d9d0b3e13e761388
ADEESA indumina <94174257+METRODENS@users.noreply.github.com>
2021-12-15 11:11:03 +0530

    Update SQ-QA.js

--- SQ-QA.js
+++ SQ-QA.js
@@ -1,5 +1,5 @@
 module.exports = {
-   METRO_DENS_OWNER: '94702506161,94718141853', // Adeesa Indumina - Metro Dens Owner
-   METRO_DENS_TEAM: '94714366177,94766911891,94711031915,94757222271,94713179802', // Team - METRO DENS TEAM 
+   METRO_BOT_OWNER: '94702506161,94718141853', // Adeesa Indumina - Metro Dens Owner
+   METRO_BOT_TEAM: '94702506161', // Team - METRO DENS TEAM 
    QUEEN_AMDI_OWNER: '94757405652' // Black Amda - Queen Amdi Owner --> For The Friendship 
     } 
